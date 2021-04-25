7a022f80c58835eb660786b065e9f84af96c4e5a
Caroline Visoto Nunes <carolvisoto@gmail.com>
2021-04-25 13:37:24 +1000

    display content with functional components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 //import the React and ReactDom
 import React from 'react';
 import ReactDom from 'react-dom';
+
+//create a react component
+
+const App = () => {
+return <div> Hi there!</div>
+}
+
+//take the react comp and show it on the screen
+
+ReactDom.render(
+<App/>,
+ document.querySelector('#root')
+)
